8e9d17f6e66ace1c26392d8fc3b076bd76be85a2
Hemanth Vennelakanti <hemanthvennelakanti@gmail.com>
2022-10-13 15:26:19 +0530

    modify header comp details

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
     declarations: [],
-    exports: [MatToolbarModule]
+    exports: [MatToolbarModule, MatButtonModule]
 })
 
 export class MaterialModule {
