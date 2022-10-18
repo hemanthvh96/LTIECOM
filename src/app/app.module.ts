f53b9ec19170b22bd375b1b40393ead313d5f8ee
Swapnali <shirkeswapnali111@gmail.com>
2022-10-18 16:42:27 +0530

    adding sign in and sign up together

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,21 @@ import { AppRoutingModule } from './app.routing.module';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SignInComponent } from './sign-in/sign-in.component';
+//import { WishlistComponent } from './wishlist/wishlist.component';
 
 import { HeaderComponent } from './header/header.component';
 import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SignupComponent
+    SignupComponent,
+    SignInComponent,
+    //WishlistComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -3,10 +3,15 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
     declarations: [],
-    exports: [MatToolbarModule, MatButtonModule, MatInputModule, MatIconModule]
+    exports: [MatToolbarModule, MatButtonModule, MatInputModule, MatIconModule, MatTabsModule, 
+             MatFormFieldModule, MatSelectModule, MatTableModule,]
 })
 
 export class MaterialModule {
